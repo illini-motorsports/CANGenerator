359ed082ab68df5c77b09ea67a97c23e7304bcc5
Rishi Masand <rishimasand@Rishis-MacBook-Pro.local>
2018-04-14 21:23:14 -0500

    assembling stringbuilder while parsing

--- src/main/java/com/illinimotorsports/model/generate/ParsedDataGenerator.java
+++ src/main/java/com/illinimotorsports/model/generate/ParsedDataGenerator.java
@@ -8,7 +8,6 @@ import com.illinimotorsports.model.canspec.CANNumericField;
 import com.illinimotorsports.model.canspec.CANSpec;
 import com.illinimotorsports.model.parse.CANLoggedMessage;
 import com.illinimotorsports.model.parse.LoggedMessages;
-
 import java.io.File;
 import java.text.DecimalFormat;
 import java.util.*;
@@ -18,6 +17,8 @@ import java.util.stream.Collectors;
 public class ParsedDataGenerator extends SelectedDataGenerator {
 
     private LoggedMessages messages;
+    private final int MAX_LINE_LENGTH = 1826;
+    private final int NUM_CHUNKS = 10000;
 
     public ParsedDataGenerator(LoggedMessages messages) {
         this.messages = messages;
@@ -57,23 +58,33 @@ public class ParsedDataGenerator extends SelectedDataGenerator {
             });
         });
 
-        StringBuilder outputBuilder = new StringBuilder();
-        outputBuilder.append("Timestamp,");
-        outputBuilder.append(String.join(",", columnNames) + '\n');
         DecimalFormat df = new DecimalFormat("#.0000");
         int numFields = columnNames.size();
 
         /*   CHUNKED IMPLEMENTATION    */
         ArrayList<Double> rowKeyList = new ArrayList<>(table.rowKeySet());
         ArrayList<ArrayList<Double>> rowKeyChunked = new ArrayList<>();
+        int CHUNK_SIZE = rowKeyList.size() / NUM_CHUNKS;
         // split data (rows) into chunks
-        final int NUM_CHUNKS = 10000;
+        Map<Double, Integer> timestampToIndexMap = new HashMap<>();
         for (int i = 0; i < NUM_CHUNKS; i++) {
             rowKeyChunked.add(sublist(rowKeyList, i * (rowKeyList.size() / NUM_CHUNKS), (i + 1) *
                     (rowKeyList.size() / NUM_CHUNKS) - 1));
+            timestampToIndexMap.put(Double.parseDouble(df.format(rowKeyList.get(i * (rowKeyList.size() /
+                    NUM_CHUNKS)
+            ))), i);
+//            System.out.println("putting: " + Double.parseDouble(df.format(rowKeyList.get(i * (rowKeyList.size() / NUM_CHUNKS)))) + " for chunk: " + i);
         }
+
         // build chunk stringbuilders in parallel
-        Set<StringBuilder> chunkStringBuilders = new HashSet<>();
+        StringBuilder outputBuilder = new StringBuilder();
+        outputBuilder.append("Timestamp,");
+        outputBuilder.append(String.join(",", columnNames) + '\n');
+        for (int i = 0; i < MAX_LINE_LENGTH * table.rowKeySet().size(); i++) {
+            outputBuilder.append("*");
+        }
+        System.out.println("size of output: " + outputBuilder.length());
+
         long start = System.nanoTime();
         rowKeyChunked.parallelStream().forEach(x -> {
             StringBuilder chunkStringBuilder = new StringBuilder();
@@ -88,64 +99,21 @@ public class ParsedDataGenerator extends SelectedDataGenerator {
                 }
                 chunkStringBuilder.append(String.join(",", row) + '\n');
             });
-            chunkStringBuilders.add(chunkStringBuilder);
+            Double chunkFirstTimestamp = Double.parseDouble(chunkStringBuilder
+                    .toString().split(",")[0]);
+//            System.out.println("chunk first timestamp: " + chunkFirstTimestamp);
+//            System.out.println("chunk number: " + timestampToIndexMap.get(chunkFirstTimestamp));
+            int chunkNumber = timestampToIndexMap.get(chunkFirstTimestamp);
+
+            int startIdx = MAX_LINE_LENGTH + (chunkNumber * CHUNK_SIZE * MAX_LINE_LENGTH);
+            int endIdx  = MAX_LINE_LENGTH + ((chunkNumber + 1) * CHUNK_SIZE * MAX_LINE_LENGTH);
+//            System.out.println(timestampToIndexMap.get(chunkFirstTimestamp) + " SI: " + startIdx);
+//            System.out.println(timestampToIndexMap.get(chunkFirstTimestamp) + " EI: " + endIdx);
+//            System.out.println("start of chunk: " + chunkStringBuilder.toString().substring(0, 4));
+            outputBuilder.replace(startIdx, endIdx, chunkStringBuilder.toString());
         });
-        System.out.println(System.nanoTime() - start);
+        System.out.println("parse time: " + (System.nanoTime() - start));
 
-        // putting back together needs work
-        start = System.nanoTime();
-        ArrayList<StringBuilder> chunkStringList = new ArrayList<>(chunkStringBuilders);
-
-        // sort based in order of first timestamp, earliest to latest timestamp
-
-        chunkStringList.sort((a, b) -> {
-            return Double.compare(Double.parseDouble(a.toString().split(",")[0]), Double
-                    .parseDouble(b.toString().split(",")[0]));
-        });
-
-        outputBuilder.append(chunkStringList.stream().reduce(new StringBuilder(), (a, b) -> a
-                .append(b)));
-
-        /*                  */
-        System.out.println(System.nanoTime() - start);
-        return outputBuilder.toString();
-
-        // ORIGINAL
-
-//        List<CANDataField> selectedFields = getData().stream().map(x -> (CANDataField) x.getData()).collect(Collectors.toList());
-//
-//        // Create sets and lists of relevant fields from the selected fields
-//        Set<Integer> selectedIDs = selectedFields.stream().map(x -> x.getNodeId()).collect(Collectors.toSet());
-//        List<String> columnNames = selectedFields.stream().map(x -> x.getName()).collect(Collectors.toList());
-//
-//        // Gather all fields from each message
-//        ImmutableMultimap.Builder<Integer, CANDataField> builder = new ImmutableSetMultimap.Builder<>();
-//        selectedFields.stream().forEach(x -> builder.put(x.getNodeId(), x));
-//        Multimap<Integer, CANDataField> fieldMultimap = builder.build();
-//
-//        Table<Double, String, Double> table = TreeBasedTable.create();
-//        messages.getMessages().stream().filter(x -> selectedIDs.contains(x.getId())).forEach(message -> {
-//            fieldMultimap.get(message.getId()).stream().forEach(field -> {
-//                table.put(message.getTimestamp(), field.getName(), message.getField(field));
-//            });
-//        });
-//
-//        StringBuilder outputBuilder = new StringBuilder();
-//        outputBuilder.append("Timestamp,");
-//        outputBuilder.append(String.join(",", columnNames) + '\n');
-//        DecimalFormat df = new DecimalFormat("#.0000");
-//        int numFields = columnNames.size();
-//        table.rowKeySet().stream().forEach(timestamp ->  {
-//            String[] row = new String[numFields + 1];
-//            row[0] = df.format(timestamp);
-//            Map<String, Double> tableRow = table.row(timestamp);
-//            for(int i = 0; i < numFields; i++) {
-//                String name = columnNames.get(i);
-//
-//                row[i+1] = tableRow.containsKey(name) ? df.format(tableRow.get(name)) : "";
-//            }
-//            outputBuilder.append(String.join(",", row) + '\n');
-//        });
-//        return outputBuilder.toString();
+        return outputBuilder.toString().replace("*", "");
     }
 }
