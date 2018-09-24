7f70f7eba4f5d34d81ef3e013e3281154bdaeb68
jake1317 <jleonrd2@illinois.edu>
2018-09-24 02:54:32 +0200

    removed build files

--- target/classes/themes/subParse.c
+++
@@ -1,9 +0,0 @@
-      {% loop in $fields %}
-      //{$comment}
-      {% if ($type == numeric) %}
-      CAN_extract_numeric(data, {$pos}, {$len}, {$endian}, {$sgn}, {$scl}, {$off});
-      {% elseIf ($type == bit) %}
-      CAN_extract_bit(data, {$pos}, {$len}, {$endian}, {$bitPos});
-      {% endif %}
-      {% onEmpty %}
-      {% endloop %}
